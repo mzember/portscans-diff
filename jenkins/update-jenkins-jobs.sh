d3a9e7de7c603dafd6c7b12cee10e7292a1a3e46
Eddie Olson <eddie.olson@liferay.com>
2017-01-05 18:35:58 -0800

    LRINFOSEC-57 Update jenkins vms w/o cron'd scripts, etc

--- jenkins/update-jenkins-jobs.sh
+++
@@ -1,7 +0,0 @@
-cd /opt/dev/projects/github/lfris-security;
-git clean -dfx;
-git reset --hard;
-git checkout --detach;
-git fetch -f upstream master:master;
-git checkout -f master;
-cp -Rf jenkins/jobs/* /opt/java/jenkins/jobs;
