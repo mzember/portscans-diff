a0a53e9b06a8e819b819dd12b8feca00d4d59a33
amosfong <amos.fong@liferay.com>
2017-10-05 14:34:09 -0700

    LRINFOSEC-178

---
+++ purple/penscan/src/main/kotlin/com/liferay/security/Rescanner.kt
@@ -0,0 +1,73 @@
+package com.liferay.security
+
+import com.liferay.security.jira.*
+
+import java.io.File
+import java.util.Properties
+import java.util.logging.Logger
+
+import org.json.JSONObject
+
+val LABEL_VULNERABLE_HOST_PREFIX = "host-vulnerability:"
+
+fun main(args: Array<String>) {
+	if (args.isEmpty()) {
+		println("You must specify a JIRA ticket. For example: \"-Pticket=LRIS-123\"")
+
+		return;
+	}
+
+	val properties = Properties()
+
+	val propertiesFile = File("penscan.properties")
+
+	properties.load(propertiesFile.inputStream())
+
+	loadExtProperties(properties, propertiesFile)
+
+	val rescanner = Rescanner(properties = properties)
+
+	rescanner.rescan(args.get(0))
+}
+
+private fun loadExtProperties(properties: Properties, propertiesFile: File) {
+	val propertiesFilePath = propertiesFile.path
+
+	val propertiesExtFilePath = propertiesFilePath.replace(Regex("\\.properties$"), "-ext.properties")
+
+	val propertiesExtFile = File(propertiesExtFilePath)
+
+	if (propertiesExtFile.exists()) {
+		properties.load(propertiesExtFile.inputStream())
+	}
+}
+
+class Rescanner(properties: Properties = Properties()) {
+	companion object {
+		val logger = Logger.getLogger(Rescanner::javaClass.name)!!
+	}
+
+	val jiraRestUtil = JiraRestUtil(properties = properties)
+	
+	fun rescan(issueKey: String) {
+		val jiraIssueJSONObject = jiraRestUtil.getIssue(issueKey)
+
+		val fieldsJSONObject = jiraIssueJSONObject.getJSONObject("fields")
+
+		val labelsJSONArray = fieldsJSONObject.getJSONArray("labels")
+
+		for (i in 0..labelsJSONArray!!.length() - 1) {
+			val label = labelsJSONArray.getString(i)
+
+			if (label.startsWith(LABEL_VULNERABLE_HOST_PREFIX)) {
+				val hostVulnerability = label.substring(LABEL_VULNERABLE_HOST_PREFIX.length)
+
+				val hostVulnerabilityArray = hostVulnerability.split(";")
+
+				val host = hostVulnerabilityArray[0]
+				val vulnerability = hostVulnerabilityArray[1]
+			}
+		}
+	}
+
+}
---
+++ purple/penscan/src/main/kotlin/com/liferay/security/jira/JiraRestUtil.kt
@@ -0,0 +1,46 @@
+package com.liferay.security.jira
+
+import com.liferay.security.*
+
+import java.io.ByteArrayOutputStream
+import java.util.Properties
+import java.util.logging.Logger
+
+import org.json.JSONObject
+
+class JiraRestUtil(properties: Properties = Properties()) {
+	companion object {
+		val logger = Logger.getLogger(JiraRestUtil::javaClass.name)!!
+	}
+
+	val properties = Properties()
+
+	init {
+		(this.properties).putAll(properties)
+	}
+
+	fun getIssue(issueKey: String): JSONObject {
+		val jiraHost: String by properties
+		val jiraPassword: String by properties
+		val jiraUsername: String by properties
+
+		val url = "https://$jiraHost/rest/api/2/issue/$issueKey"
+
+		val byteArrayOutputStream = ByteArrayOutputStream()
+
+		exec("curl", listOf("--silent", "--user", "$jiraUsername:$jiraPassword", "--header", "Content-Type: application/json", url), standardOutput = byteArrayOutputStream, skipPrintCommandLine = true)
+
+		val jsonObject = JSONObject(byteArrayOutputStream.toString())
+
+		logger.finest(jsonObject.toString())
+
+		if (jsonObject.has("errorMessages")) {
+			val jsonArray = jsonObject.getJSONArray("errorMessages")
+
+			throw Exception(jsonArray.toString())
+		}
+
+		return jsonObject
+	}
+
+}
