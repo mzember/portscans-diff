64e09633e0dc234df0f86feab3c7d6e4e0812afc
Eddie Olson <eddie.olson@liferay.com>
2017-12-07 15:31:49 -0800

    Update default credentials test for LRINFOSEC-220

--- purple/penscan/src/main/resources/vulnerabilities/liferay/LRINFOSEC-121.sh
+++ purple/penscan/src/main/resources/vulnerabilities/liferay/LRINFOSEC-121.sh
@@ -6,4 +6,10 @@ if echo "$VAR" | grep -q '/default_credentials'; then
     exitValue=0
 fi
 
+VAR="$(curl -s -L -k -m 3 --include "http://$1/c/portal/login" --data "login=test&password=test&redirect=/default_credentials" -H "Cookie: COOKIE_SUPPORT=true")"
+
+if echo "$VAR" | grep -q '/default_credentials'; then
+    exitValue=0
+fi
+
 exit $exitValue
