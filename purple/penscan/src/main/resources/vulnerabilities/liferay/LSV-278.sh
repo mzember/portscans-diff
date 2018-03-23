d3e19a5ab19dc90ec0645b4f38860d661e036eaa
Eddie Olson <eddie.olson@liferay.com>
2018-03-23 14:51:00 -0700

    LRINFOSEC-223 Source formatting - Apply alphabetical pattern

--- purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-278.sh
+++ purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-278.sh
@@ -1,6 +1,6 @@
 exitValue=1
 
-VAR="$(curl -s -L -m 3 "http://$1/api//axis")"
+VAR="$(curl -L -m 3 -s "http://$1/api//axis")"
 
 if echo "$VAR" | grep -q 'Portal_PortalService'; then
     exitValue=0
--- purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-340.sh
+++ purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-340.sh
@@ -1,11 +1,11 @@
 exitValue=1
 
-VAR="$(curl -s -L -m 3 "http://$1/wsrp-portlet/proxy/a?url=http://$1/api/axis")"
+VAR="$(curl -L -m 3 -s "http://$1/wsrp-portlet/proxy/a?url=http://$1/api/axis")"
 
 if echo "$VAR" | grep -q 'Portal_PortalService'; then
     exitValue=0
 else
-    VAR="$(curl -s -L -m 3 "http://$1/o/wsrp-service/proxy/a?url=http://$1/api/axis")"
+    VAR="$(curl -L -m 3 -s "http://$1/o/wsrp-service/proxy/a?url=http://$1/api/axis")"
 
     if echo "$VAR" | grep -q 'Portal_PortalService'; then
         exitValue=0
