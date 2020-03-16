e0810d99e36aee3211b453f603c4198c0d9c03b2
Martin Žember <zember@gmail.com>
2020-03-16 14:24:33 +0100

    xml is enough

--- run-nmap-Tfast-polite.sh
+++ run-nmap-Tfast-polite.sh
@@ -1,5 +1,5 @@
 OUTPUT=`basename $0 | sed 's/run-//;s/\.sh$//'`
 test -n "$OUTPUT" || exit 1
-exec nmap -A -Pn -F -iL Targets.IP -oA "$OUTPUT" --privileged -vvv -T polite
+exec nmap -A -Pn -F -iL Targets.IP -oX "$OUTPUT" --privileged -vvv -T polite
 #--max-scan-delay 0.4s --host-timeout 30m
 
