fa2c1dcaf3db59e63a7c572e294f7d9a95d23d1f
Martin Žember <zember@gmail.com>
2020-03-16 14:24:07 +0100

    TEST_IP var

--- newscan.sh
+++ newscan.sh
@@ -1,7 +1,11 @@
 DIR=scan-`date +%Y-%m-%d-%H:%M:%S`
 
 mkdir "$DIR"
-cp Targets.IP "$DIR"
+if [ -n "$TEST_IP" ]; then
+    echo "$TEST_IP" > "$DIR/Targets.IP"
+else 
+    cp Targets.IP "$DIR"
+fi
 (
 cd "$DIR"
 sh -x ../run-nmap-Tfast-test.sh
